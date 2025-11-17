314599b156bd29f2fd962b4446551efce2b339ce
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-17 20:47:15 +0100

    buildArray decl

--- Sources/Constructors/css/css-decl-builder.swift
+++ Sources/Constructors/css/css-decl-builder.swift
@@ -5,6 +5,11 @@ public enum CSSDeclBuilder {
     public static func buildBlock(_ parts: CSSDeclaration...) -> [CSSDeclaration] {
         parts
     }
+
+    public static func buildArray(_ parts: [[CSSDeclaration]]) -> [CSSDeclaration] {
+        parts.flatMap { $0 }
+    }
+
     public static func buildExpression(_ decl: CSSDeclaration) -> [CSSDeclaration] { [decl] }
     public static func buildExpression(_ decls: [CSSDeclaration]) -> [CSSDeclaration] { decls }
 }
